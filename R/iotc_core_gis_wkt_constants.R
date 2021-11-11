da819d8ad4355abd664d5b91292d28e4c25685f4
flowerside <flowerside@gmail.com>
2021-11-11 11:46:49 +0400

    Initial commit (v0.2.0)

---
+++ R/iotc_core_gis_wkt_constants.R
@@ -0,0 +1,35 @@
+#' IOTC EIO - TO BE REDEFINED
+#' @export
+IO_EAST = "IREASIO"
+
+#' IOTC WIO - TO BE REDEFINED
+#' @export
+IO_WEST = "IRWESIO"
+
+#' IOTC large areas (West/East) - TO BE REDEFINED
+#' @export
+IO_BOTH = c("IRWESIO", "IREASIO")
+
+#' IOTC all IO (single area) - TO BE REDEFINED
+#' @export
+IO_ALL = "IRALLIO"
+
+#' IOTC high seas - TO BE REDEFINED
+#' @export
+IO_HS = "IRALLHS"
+
+#' IOTC all EEZs (single area) - TO BE REDEFINED
+#' @export
+IO_EEZs = "IRALLEZ"
+
+#' FAO area 51 (Western IO)
+#' @export
+IO_F51 = "IRFAO51"
+
+#' FAO area 57 (Eastern IO)
+#' @export
+IO_F57 = "IRFAO57"
+
+#' FAO AREA 51 and 57 combined (best representation of IOTC area)
+#' @export
+IO_F5157 = c("IRFAO51", "IRFAO57")
